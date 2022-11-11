4905cf2ef79c0cc8f66a173659010aba7a012a81
Oleg Krohmal <krohmal_oleg86@mail.ru>
2022-11-11 13:48:26 +0300

    JS2Dz1

---
+++ main.js
@@ -0,0 +1,58 @@
+//_____________________1____________________________
+
+
+// function printArrayValues(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//      console.log(arr[i], 'array');
+//     }
+//    }
+   
+// printArrayValues([1, 2, 3]);
+
+
+// let count = 0
+// function printArrayValues(arr) {
+//     count++;
+//     console.log(count, 'array');
+//     if(count !== arr.length){
+//         return printArrayValues(arr)
+//     }else{
+//         return 
+//     }
+// }
+// printArrayValues([1,2,3])
+
+
+//_____________________2____________________________
+
+
+// let arr = [4, 5, 6]
+// arr.unshift(1,2,3)
+// arr.push(7, 8, 9, 10)
+// console.log(arr);
+
+// let arr = [4, 5, 6]
+// let newArr = arr.concat([7, 8, 9, 10])
+// newArr.unshift(1,2,3)
+// console.log(newArr);
+
+
+//_____________________3____________________________
+
+
+
+// let arr = [1, 2, 3, 4, 5]
+// console.log(arr.slice(4));
+
+
+
+//_____________________4____________________________
+
+
+// let count = 'loremipsumdolor'
+//   function countChar() {
+//     let RegExp = /o/gi;
+//     let array = count.match(RegExp);
+//     console.log(array);
+//  }
+// countChar()
